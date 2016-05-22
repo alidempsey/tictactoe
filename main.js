628aa938182eb88e2cd87755f062c9873f378e2b
Ali Dempsey <alidempsey@gmail.com>
2016-05-21 22:11:41 -0400

    tables are dumb. two_player cells can be clicked and image of 'o' appears

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ function two_player () {
     document.getElementById("two_player").onclick = function () {
         document.getElementById("start_game").style.display = "none";
         console.log("start_game element is now hidden");
-        document.getElementById("grid").style.display = "table";
+        document.getElementById("grid").style.display = "block";
     }
+    var cell = document.getElementsByClassName("cell")
+    for (var i = 0; i < 9; i++) {
+        cell[i].onclick = function () {
+            this.style.background = "url('img/o.png')";
+            this.style.backgroundSize = "contain";
+            console.log("clicked a cell");
+        }
+    }
+    
 }
